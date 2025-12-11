76e5d12bd0f332dbf559bccea814a20df686a901
Caden F <cfinke895@west-mec.edu>
2025-12-11 13:58:48 -0700

    asdas

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::process::Command;
 use terminal_menu::*;
-use crossterm::style::Color;
+use colored_text::Colorize;
 
 const PROGRAM_TITLE: &str = "West-Mec Repair Shop Configuration Tool";
 const INSTALL_TYPES: &[&str] = &[
@@ -9,12 +9,25 @@ const INSTALL_TYPES: &[&str] = &[
     "Remove Installed Programs (from this script)",
     "Remove Unecessary Programs (or bad ones)",
 ];
+const INSTALL_PROGRAMS: &[&str] = &[
+    "NZXT.CAM",
+    "WhirlwindFX.SignalRgb",
+    "CrystalRich.LockHunter",
+    "Klocman.BulkCrapUninstaller",
+    "valinet.ExplorerPatcher",
+    "Git.Git",
+    "Hibbiki.Chromium",
+    "GitHub.GitHubDesktop",
+    "Microsoft.VisualStudioCode",
+    "Microsoft.VisualStudio.2019.BuildTools"
+];
+
 
 fn main() {
     if cfg!(target_os = "windows") {
 
         let menu = menu(vec![
-            label(PROGRAM_TITLE).colorize(Color::Yellow),
+            label(format!("{}", PROGRAM_TITLE).hex("F57E20")),
             label(""),
             scroll("Install Type", INSTALL_TYPES.iter().copied()),
             label(""),
@@ -22,11 +35,13 @@ fn main() {
         ]);
         run(&menu);
         {
-            let mm = mut_menu(&menu);
-            println!("{}", mm.selection_value("Install Type"));
-            println!("{}", mm.selected_item_name());
+            if !mut_menu(&menu).canceled() == true {
+                let mm = mut_menu(&menu);
+                println!("{}", mm.selection_value("Install Type"));
+                println!("{}", mm.selected_item_name());
 
-            handle_install_type(mm.selection_value("Install Type"));
+                handle_install_type(mm.selection_value("Install Type"));
+            }
         }
     } else {
         Command::new("clear")
@@ -36,30 +51,75 @@ fn main() {
 }
 
 fn handle_install_type(_type: &str) {
-    if (_type == INSTALL_TYPES[0]) {
-        println!("Hello!!!!");
+    if _type == INSTALL_TYPES[0] {
+        handle_run_install_full();
+    } else if _type == INSTALL_TYPES[1] {
+        handle_run_install_programs();
+    } else if _type == INSTALL_TYPES[2] {
+        handle_run_remove_installed_programs();
     } else {
-        println!("Goodbye!!!!");
+        println!("{}", "Invalid Entry (idk what is wrong)".hex("FF0000"));
     }
 }
 
 fn winget_install(program_name: &str) {
-    let installing_message: String = format!("Installing {}...", program_name);
-    let install_message_command: String = format!("Write-Host {}", installing_message);
+    let installing_message: String = format!("Installing {}...", program_name.hex("F57E20"));
+    let install_command: String = format!("winget install -e --id {} --silent --disable-interactivity --accept-package-agreements --accept-source-agreements", program_name);
 
-    let install_message_command_output = Command::new("powershell")
+    println!("{}\n", installing_message);
+
+    let install_command_output = Command::new("powershell")
         .arg("-Command")
-        .arg(install_message_command)
+        .arg(install_command)
         .output()
-        .expect("Failed to send \"Installing\" message...");
-    
-    String::from_utf8_lossy(&install_message_command_output.stdout);
+        .expect("Failed to install program via winget...");
 
-    if !install_message_command_output.status.success() {
-        eprintln!("PowerShell returned an error:\n{}", String::from_utf8_lossy(&install_message_command_output.stderr));
+    if !install_command_output.status.success() {
+        eprintln!("{}", format!("PowerShell returned an error:\n{}", String::from_utf8_lossy(&install_command_output.stdout)).hex("FF0000"));
+    } else {
+        println!("{}", String::from_utf8_lossy(&install_command_output.stdout));
     }
 }
 
-// fn install_pograms() {
-    
-// }
+fn winget_remove(program_name: &str) {
+    let removing_message: String = format!("Removing {}...", program_name.hex("FF0000"));
+    let remove_command: String = format!("remove -e --id {}", program_name);
+
+    println!("{}\n", removing_message);
+
+    let remove_command_output = Command::new("powershell")
+        .arg("-Command")
+        .arg(remove_command)
+        .output()
+        .expect("Failed to remove program via winget...");
+
+    if !remove_command_output.status.success() {
+        eprintln!("{}", format!("PowerShell returned an error:\n{}", String::from_utf8_lossy(&remove_command_output.stdout)).hex("FF0000"));
+    } else {
+        println!("{}", String::from_utf8_lossy(&remove_command_output.stdout));
+    }
+}
+
+fn install_programs() {
+    for program in INSTALL_PROGRAMS {
+        winget_install(program);
+    }
+}
+
+fn handle_run_install_full() {
+    println!("Starting Full Install...");
+
+    install_programs();
+}
+
+fn handle_run_install_programs() {
+    println!("Starting Programs Install...");
+
+    install_programs();
+}
+
+fn handle_run_remove_installed_programs() {
+    for program in INSTALL_PROGRAMS {
+        winget_remove(program);
+    }
+}
