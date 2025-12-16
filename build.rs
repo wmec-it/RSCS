96ed4e071b74340de7870cd55c6b9a7cfbf7310f
Caden F <cfinke895@west-mec.edu>
2025-12-16 13:53:16 -0700

    sd

--- build.rs
+++ build.rs
@@ -4,9 +4,22 @@ use {
 };
 
 fn main() -> io::Result<()> {
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=assets/punchdown_paul.ico");
+
+    let version = env::var("CARGO_PKG_VERSION").unwrap();
+
     if env::var_os("CARGO_CFG_WINDOWS").is_some() {
         WindowsResource::new()
             .set_icon("assets/punchdown_paul.ico")
+            .set("FileDescription", "Repair Shop Configuration Superintendant CLI program.")
+            .set("ProductName", "Repair Shop Configuration Superintendant")
+            .set("CompanyName", "West-Mec Southwest, IT Security")
+            .set("LegalCopyright", "© 2025 West-Mec Southwest, IT Security")
+            .set("OriginalFilename", "rscs.exe")
+            .set("InternalName", "rscs")
+            .set("FileVersion",  &format!("{}.0", version))
+            .set("ProductVersion", &version)
             .compile()?;
     }
     Ok(())
