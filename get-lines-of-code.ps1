7e57a2486fd139e5661ddad1d96dd91c3bf6ff7c
Caden F <cfinke895@west-mec.edu>
2025-12-18 14:55:55 -0700

    Fixed

--- get-lines-of-code.ps1
+++ get-lines-of-code.ps1
@@ -1,4 +1,1 @@
-@echo off
-
-@REM Counts lines of .rs file code.
 Get-ChildItem -Filter "*.rs" -Recurse | Get-Content | Where-Object { $_.Trim() -notlike "//" -and $_.Trim() -ne "" } | Measure-Object -Line
