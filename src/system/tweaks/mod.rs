bc608fa84035d1cc2fa7074412611dcf42347655
Caden F <121118774+MysticalMike60t@users.noreply.github.com>
2026-01-01 18:52:12 -0800

    Power plan

--- src/system/tweaks/mod.rs
+++ src/system/tweaks/mod.rs
@@ -5,6 +5,7 @@ use crate::{
 
 pub mod powershell;
 pub mod registry;
+pub mod power;
 
 pub fn run_tweaks() {
     message::success(
@@ -20,11 +21,17 @@ pub fn run_tweaks() {
         .as_str(),
     );
 
+    // Powershell Tweaks
     powershell::ps7::full();
     powershell::ps7telemetry::disable();
+
+    // Registry Tweaks
     registry::rclick_end_task::enable();
     registry::prefer_ipv4::enable();
     registry::bingsearch_startmenu::disable();
     registry::darkmode::enable();
     registry::explorerpatcher_config::enable();
+
+    // Power Tweaks
+    power::ultimate_powerplan::enable();
 }
