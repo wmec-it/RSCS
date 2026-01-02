b5cb768f4ca7c575a2b4ea4508b76c6888300606
Caden F <121118774+MysticalMike60t@users.noreply.github.com>
2026-01-01 19:48:55 -0800

    Templates

--- src/system/tweaks/registry/taskview_button.rs
+++ src/system/tweaks/registry/taskview_button.rs
@@ -1,17 +1,14 @@
 // https://winutil.christitus.com/dev/tweaks/customize-preferences/taskview/
 
-use crate::{
-    conf::enums::{DelimiterType, MessageType},
-    utils::message,
-};
-use std::process::Command;
+use crate::system::tweaks::templates;
 
 #[allow(dead_code)]
 pub fn enable() {
-    let run_message: String = "Enabling Taskbar Task View Button...".to_string();
-    let success_message: String = "Successfully enabled Taskbar Task View Button!".to_string();
-    let error_message: String = "Failed to disable Taskbar Task View Button...".to_string();
-    let command: &str = "try {
+    templates::default(
+        "Enabling Taskbar Task View Button...",
+        "Successfully enabled Taskbar Task View Button!",
+        "Failed to disable Taskbar Task View Button...",
+        "try {
        $value = 1
         $Path = \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced\"
         Set-ItemProperty -Path $Path -Name ShowTaskViewButton -Value $value
@@ -22,60 +19,17 @@ pub fn enable() {
     } catch {
         Write-Warning \"Unable to set $Name due to unhandled exception\"
         Write-Warning $psitem.Exception.StackTrace
-    }";
-
-    message::normal(
-        MessageType::Print,
-        message::add_delimiter(DelimiterType::Layer1, run_message, Some(true), None, None)
-            .unwrap()
-            .as_str(),
+    }",
     );
-    let output = Command::new("powershell")
-        .arg("-Command")
-        .arg(command)
-        .output()
-        .expect("Failed to run PowerShell");
-
-    if output.status.success() {
-        message::success(
-            MessageType::Print,
-            message::add_delimiter(
-                DelimiterType::Layer2Success,
-                success_message,
-                Some(true),
-                None,
-                Some(true),
-            )
-            .unwrap()
-            .as_str(),
-        );
-    } else {
-        message::error(
-            MessageType::Print,
-            message::add_delimiter(
-                DelimiterType::Layer2Error,
-                format!(
-                    "{}\nExit Code: {:?}\n{}",
-                    error_message,
-                    output.status.code(),
-                    String::from_utf8_lossy(&output.stderr)
-                ),
-                Some(true),
-                None,
-                Some(true),
-            )
-            .unwrap()
-            .as_str(),
-        );
-    }
 }
 
 #[allow(dead_code)]
 pub fn disable() {
-    let run_message: String = "Disabling Taskbar Task View Button...".to_string();
-    let success_message: String = "Successfully disabled Taskbar Task View Button!".to_string();
-    let error_message: String = "Failed to Taskbar Task View Button...".to_string();
-    let command: &str = "try {
+    templates::default(
+        "Disabling Taskbar Task View Button...",
+        "Successfully disabled Taskbar Task View Button!",
+        "Failed to disable Taskbar Task View Button...",
+        "try {
        $value = 0
         $Path = \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced\"
         Set-ItemProperty -Path $Path -Name ShowTaskViewButton -Value $value
@@ -86,50 +40,6 @@ pub fn disable() {
     } catch {
         Write-Warning \"Unable to set $Name due to unhandled exception\"
         Write-Warning $psitem.Exception.StackTrace
-    }";
-
-    message::normal(
-        MessageType::Print,
-        message::add_delimiter(DelimiterType::Layer1, run_message, Some(true), None, None)
-            .unwrap()
-            .as_str(),
+    }",
     );
-    let output = Command::new("powershell")
-        .arg("-Command")
-        .arg(command)
-        .output()
-        .expect("Failed to run PowerShell");
-
-    if output.status.success() {
-        message::success(
-            MessageType::Print,
-            message::add_delimiter(
-                DelimiterType::Layer2Success,
-                success_message,
-                Some(true),
-                None,
-                Some(true),
-            )
-            .unwrap()
-            .as_str(),
-        );
-    } else {
-        message::error(
-            MessageType::Print,
-            message::add_delimiter(
-                DelimiterType::Layer2Error,
-                format!(
-                    "{}\nExit Code: {:?}\n{}",
-                    error_message,
-                    output.status.code(),
-                    String::from_utf8_lossy(&output.stderr)
-                ),
-                Some(true),
-                None,
-                Some(true),
-            )
-            .unwrap()
-            .as_str(),
-        );
-    }
 }
