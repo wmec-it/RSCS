5befda759842e0218a1b9fdaf2ba22b9bfa31593
Caden F <121118774+MysticalMike60t@users.noreply.github.com>
2026-01-14 01:13:57 -0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ fn main() -> Result<(), io::Error> {
         // Exit
         Ok(())
     } else {
-        // Show error
+        // Show error for nerds on Linux
         message::error(
             MessageType::Print,
             message::add_delimiter(
@@ -58,14 +58,18 @@ fn main() -> Result<(), io::Error> {
     }
 }
 
+// Opens menu
 fn open_menu(operation_type: &str) -> Result<(), io::Error> {
     match operation_type {
         "testing" => {
+            // Lets you use the PROJECT_ROOT/src/testing/testing.rs file for testing stuff
             testing();
             Ok(())
         }
         "skip" => {
+            // Enables sudo
             user::enable_sudo();
+            // Run prerequisites and catch errors
             match prerequisites() {
                 Ok(()) => (),
                 Err(error) => {
@@ -84,10 +88,12 @@ fn open_menu(operation_type: &str) -> Result<(), io::Error> {
                     return Err(error);
                 }
             }
+            // Handle install type selection
             handles::install_type("Skip all tweaks");
             Ok(())
         }
         _ => {
+            // Menu layout definition
             let menu = menu(vec![
                 label(format!("{}", PROGRAM_TITLE).hex(MAIN_THEME.primary)),
                 label(""),
@@ -95,8 +101,10 @@ fn open_menu(operation_type: &str) -> Result<(), io::Error> {
                 label(""),
                 button("Start Install"),
             ]);
+            // I don't need to explain this...
             run(&menu);
             {
+                // Catch if there is an oopsie in the menu logic
                 match menu_logic(menu) {
                     Ok(()) => (),
                     Err(error) => println!("{}", error),
@@ -111,22 +119,21 @@ fn menu_logic(
     menu: std::sync::Arc<std::sync::RwLock<TerminalMenuStruct>>,
 ) -> Result<(), io::Error> {
     if !mut_menu(&menu).canceled() == true {
+        // Prints the orange god
         println!("{}", format!("{}", PUNCHDOWN_PAUL).hex(MAIN_THEME.primary));
 
-        println!(
-            "{}{}",
-            message
-                ::error_banner(
-                    MessageType::Return,
-                    "   Make sure you have permission from Mr. Getz if you are using this program...    "
-                )
-                .unwrap(),
-            "\n"
+        // Warning banner to make sure Mr. Getz doesn't smite you
+        message::error_banner(
+            MessageType::Print,
+            "   Make sure you have permission from Mr. Getz if you are using this program...    ",
         );
 
+        // Obviously enables sudo
         user::enable_sudo();
 
         let mm = mut_menu(&menu);
+        // Displays install type
+        // Using println!("{} {}") So that the selection value is just normal text color but formatting is the same
         println!(
             "{} {}",
             message::info(
@@ -145,18 +152,23 @@ fn menu_logic(
             mm.selection_value("Install Type")
         );
 
+        // Run prerequisites and catch errors
         match prerequisites() {
             Ok(()) => (),
             Err(error) => {
+                // Display error message
                 utils::errors::function("Prerequisites failed to run...");
                 return Err(error);
             }
         }
 
+        // Handle install type selection
         handles::install_type(mm.selection_value("Install Type"));
 
+        // Everything is fine :3
         Ok(())
     } else {
+        // Oopsy woopsie, there is an error :(
         Err(io::Error::new(
             io::ErrorKind::Other,
             "Failed to handle menu logic...",
@@ -165,9 +177,12 @@ fn menu_logic(
 }
 
 fn prerequisites() -> Result<(), io::Error> {
+    // Handle errors from restore point
     match system::manage::backups::create_restore_point() {
+        // Everything is ok
         Ok(()) => Ok(()),
         Err(error) => {
+            // Display error message
             utils::errors::function("Failed to handle menu logic...");
             return Err(error);
         }
