23cb8866850b31b109025bbbd75b634e19e36c79
Caden F <cfinke895@west-mec.edu>
2026-01-30 13:13:26 -0700

    s

---
+++ lib/move_vc_runtime.sh
@@ -0,0 +1,2 @@
+# TODO: Add test for if regular release directory exists, and if it should be copied to both dirs, or just one
+cp -r "./vc_runtime.bat" "./target/x86_64-pc-windows-gnu/release/vc_runtime.bat"
