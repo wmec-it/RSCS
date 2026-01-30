324b494bb58cad7d2056e33f9883daf7deb1e374
Butter <121118774+MysticalMike60t@users.noreply.github.com>
2026-01-30 05:21:13 -0700

    Updates and fixed linux dev issue

--- lib/move_json_files.sh
+++ lib/move_json_files.sh
@@ -1,2 +1,2 @@
 # TODO: Add test for if regular release directory exists, and if it should be copied to both dirs, or just one
-cp -r "./menu_options" "./target/x86_64-pc-windows-msvc/release/"
+cp -r "./menu_options" "./target/x86_64-pc-windows-gnu/release/"
