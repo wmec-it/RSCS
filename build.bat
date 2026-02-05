deeb880dbb58e909ae75efeb90de452b433b48a4
Caden F <cfinke895@west-mec.edu>
2026-02-05 13:53:05 -0800

    s

--- build.bat
+++ build.bat
@@ -7,4 +7,13 @@ rustup target add x86_64-pc-windows-msvc
 cargo build --target x86_64-pc-windows-msvc
 
 @REM Move JSON files into built release folder
+powershell Unblock-File -Path ./lib/move_json_files.ps1
 powershell ./lib/move_json_files.ps1
+
+@REM Move assets into built release folder
+powershell Unblock-File -Path ./lib/move_assets.ps1
+powershell ./lib/move_assets.ps1
+
+@REM Move vc_runtime.bat into built release folder
+powershell Unblock-File -Path ./lib/move_vc_runtime.ps1
+powershell ./lib/move_vc_runtime.ps1
--- format.bat
+++ format.bat
@@ -3,6 +3,8 @@
 @REM Formats code with default formatter
 cargo fmt
 
+powershell Unblock-File -Path ./lib/json_formatting.ps1
 powershell ./lib/json_formatting.ps1
 
+powershell Unblock-File -Path ./lib/convert_endings.ps1
 powershell ./lib/convert_endings.ps1
